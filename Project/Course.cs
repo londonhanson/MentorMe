2dd891871851dd9ed2b36a8ef0f6b2fec79e2345
londonhanson <lahanso3@asu.edu>
2020-04-09 15:01:04 -0700

    Adding array and functions for GetCourses
    Ability to display all courses

---
+++ Project/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace accountmanager
+{
+    public class Course
+    {
+        public int classId;
+        public int mentorId;
+        public string className;
+        public string classDesc;
+    }
+}
