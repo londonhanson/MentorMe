ed46cfba4e5565726be173f96855694fa52d0a54
Zihao Yan <zyan33@asu.edu>
2020-04-09 19:24:09 -0700

    Search Bar added.

--- Project/AccountServices.js
+++ Project/AccountServices.js
@@ -402,3 +402,23 @@ function LoadCourses() {
         }
     });
 }
+
+
+function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchbox");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("courseTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
